a061ae3bf49ede7000ebf33596f96ff86a9e8b1f
Avery Wood <awood73@charlotte.edu>
2025-02-17 16:46:05 -0500

    Feat: Adding the bad code subtraction method. Including bad practice of documentation

--- bad_code.py
+++ bad_code.py
@@ -1,6 +1,14 @@
 #Sirin-Addition-Kiss
 
-#Avery-Subtraction-Documentation
+
+# Variable for number 1
+a = input("Enter a number: ")
+# Variable for number 2
+b = input("Enter another number: ")
+# Function to complete math.
+def m(a, b):
+    # Return a - b
+    return a - b
 
 #Yana-Multiplication-Dry
 
