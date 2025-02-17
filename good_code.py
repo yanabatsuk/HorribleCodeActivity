650563e53634b02de94f153f17df235243ed2c0c
Avery Wood <awood73@charlotte.edu>
2025-02-17 16:47:44 -0500

    Feat: Adding good documentation practice for the subtraction method.

--- good_code.py
+++ good_code.py
@@ -1,6 +1,12 @@
 #Sirin-Addition-Kiss
 
-#Avery-Subtraction-Documentation
+
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+# A function to return the subtraction of the users 2 numbers.
+def subtraction(number1, number2):
+    return number1 - number2
+
 
 #Yana-Multiplication-Dry
 
