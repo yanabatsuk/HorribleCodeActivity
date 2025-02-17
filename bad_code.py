beac103b766d4235bc5e380b6d5925dbcfd4dcd5
sirinhb <sblan@charlotte.edu>
2025-02-17 16:50:57 -0500

    feat: implemented addition function that violates KISS principle

--- bad_code.py
+++ bad_code.py
@@ -1,4 +1,15 @@
-#Sirin-Addition-Kiss
+def addition(num1, num2):
+    #verifying input type
+    if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+        #intializing sum variable
+        sum=0
+        #adding first number to sum
+        sum=sum+num1
+        #adding second number to sum
+        sum=sum+num2
+        return sum
+    else:
+        return "invalid input"
 
 #Avery-Subtraction-Documentation
 
